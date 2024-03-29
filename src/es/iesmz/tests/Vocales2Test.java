980d3a149245c5cb7a252da567d0d964ea182b18
Juanvi <juanvi1921.cursos@gmail.com>
2024-03-29 15:13:24 +0100

    Commit con la tarea 3 completa

--- src/es/iesmz/tests/Vocales2Test.java
+++ src/es/iesmz/tests/Vocales2Test.java
@@ -11,8 +11,21 @@ class Vocales2Test {
 
     @Test
     void sinVocalesTest2() {
-        assertEquals("st s n cdn", Vocales2.sinVocales("Esto es una cadena"));
+        assertEquals("st s n cdn", Vocales2.sinVocales("Esto Es una cadenA"));
     }
 
+    @Test
+    void sinVocalesTest3() {
+        assertEquals("Jv ml Mglln", Vocales2.sinVocales("Java mola MogollOn"));
+    }
 
+    @Test
+    void sinVocalesTest4() {
+        assertEquals("4h0r4 s0n nm3r0s", Vocales2.sinVocales("4h0r4 s0n num3r0s"));
+    }
+
+    @Test
+    void sinVocalesTest5() {
+        assertEquals("Cdn Sn Vcls", Vocales2.sinVocales("Cadena Sin Vocales"));
+    }
 }
